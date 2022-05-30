0ad2711bcdc46a9fc1d57f173c820191393ad291
Carolina Bartoli <caculeka@hotmail.com>
2022-05-30 14:52:38 -0300

    Get e Post Finalizados

--- src/com/httpexamples/getRequest.java
+++ src/com/httpexamples/getRequest.java
@@ -4,11 +4,14 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.time.Duration;
 
 public class getRequest {
     //url do request
     public static final String uRL_GET = "https://jsonplaceholder.typicode.com/posts";
+//    public static final String uRL_GET = "http://httpbin.org/get";
+
 
     public static void main(String[] args) throws IOException, InterruptedException {
         //cliente http
@@ -20,5 +23,12 @@ public class getRequest {
                 .timeout(Duration.ofSeconds(10))
                 .uri(URI.create(uRL_GET))
                 .build();
+
+        //enviar uma solicitaçao
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        //imprimir o conteudo recebido
+        System.out.println(response.statusCode());
+        System.out.println(response.body());
     }
 }
